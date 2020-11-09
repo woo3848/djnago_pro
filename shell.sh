c6f5af1abebeef1643acc2dc1ef5a9003c2910d9
woo3848 <eee7426@gmail.com>
2020-11-09 14:09:15 +0900

    tester

--- shell.sh
+++ shell.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hello World"
+git config --global user.name "woo3848"
+git config --global user.email "eee7426@gmail.com"
+
+mkdir -p /root/best
+
+cd /root/best
+
+git init
+
+git remote add origin https://github.com/woo3848/djnago_pro.git
+
+git clone https://github.com/woo3848/djnago_pro.git /root/best
